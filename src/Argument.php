3440188d8731675b86b8abe69f9d6895a44a7e65
Marijn Ophorst <marijn@sensimedia.nl>
2018-10-29 14:06:47 +0100

    is now an extension

--- src/Argument.php
+++ src/Argument.php
@@ -1,45 +1,33 @@
 <?php
 
-namespace Codger\Javascript;
+namespace Codger\Es6;
 
+use Codger\Javascript;
 use Twig_Environment;
-use ReflectionParameter;
 
-class Argument extends Recipe
+class Argument extends Javascript\Argument
 {
     use Doccomment;
-
-    /** @var string */
-    protected $template = 'argument.html.twig';
 
     /**
      * @param Twig_Environment $twig
      */
     public function __construct(Twig_Environment $twig, string $name = null, string $default = null, bool $variadic = false)
     {
-        parent::__construct($twig);
-        $this->variables = (object)compact('variadic', 'default', 'name');
+        parent::__construct($twig, $name);
+        $this->set('default', $variadic);
+        $this->set('variadic', $variadic);
     }
 
     /**
      * Mark the argument as variadic (or not).
      *
      * @param bool $variadic Defaults to `true`
-     * @return Codger\Javascript\Argument Itself
+     * @return Codger\Es6\Argument Itself
      */
     public function isVariadic(bool $variadic = true) : Argument
     {
         return $this->set('variadic', $variadic);
-    }
-
-    /**
-     * Get the name of the parameter (if set, else null).
-     *
-     * @return string|null
-     */
-    public function getName() :? string
-    {
-        return $this->get('name');
     }
 }
 
